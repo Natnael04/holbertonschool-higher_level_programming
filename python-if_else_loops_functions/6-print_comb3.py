27a31097c2adb22bea7ad5c23400777ead020795
Natnael04 <natnaeldeboch@gmail.com>
2023-01-19 05:39:35 -0800

    adding file task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 90):
+for i in range(1, 89):
   if i % 10 != 0 and i % 11 != 0:
     if i < (i % 10) * 10 + i // 10:
       print("{:02d}".format(i), end=', ')
     
-print("{:02d}\n".format(i), end=' ')
+print("89")
