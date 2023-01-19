ba32e8b844faecbe7854f7f6b96edd41f9a4990e
Natnael04 <natnaeldeboch@gmail.com>
2023-01-19 08:22:01 -0800

    adding file task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(s):
-     for c in s:
-          if c.islower():
-               return True
-          else:
-               return False
+    for c in s:
+        if c.islower():
+            return True
+        else:
+            return False
