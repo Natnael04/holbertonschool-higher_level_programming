ee184a85559b59a633a12de70bcffd3cf3267c7d
Natnael04 <natnaeldeboch@gmail.com>
2023-01-23 03:38:31 -0800

    adding file task 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for ele in range(0, len(my_list)):
+        if idx < 0 and idx > my_list:
+            return None
+        else:
+            return my_list[idx]
