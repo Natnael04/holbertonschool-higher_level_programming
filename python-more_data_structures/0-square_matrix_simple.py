f16338ce74a9e7d917586f0419cc23722789341d
Natnael04 <natnaeldeboch@gmail.com>
2023-01-27 01:49:30 -0800

    adding file task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for column in row:
-            new_row.append(new_matrix)
-            new_matrix.append(column**2)
-    return new_matrix
-    
-    
-    
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
