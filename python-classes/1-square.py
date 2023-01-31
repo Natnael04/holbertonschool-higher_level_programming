91855ebadc9028eb9914b14be2f3ec4291cc5a97
Natnael04 <natnaeldeboch@gmail.com>
2023-01-31 02:29:58 -0800

    adding file task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# This function is creating new empty class
+"""creating a class square to define square"""
+
+
+class Square:
+    """creating a class tha define a square by private instance attribute."""
+
+    def __init__(self, size):
+        self.__size = size
