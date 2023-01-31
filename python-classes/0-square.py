8f711703b51eb6bce880dd775f6638e009b5efde
Natnael04 <natnaeldeboch@gmail.com>
2023-01-31 02:01:44 -0800

    adding file task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-#This function is creating new empty class
+# This function is creating new empty class
 """creating an empty class to define square"""
+
+
 class Square:
     """creating an empty class.
     """
