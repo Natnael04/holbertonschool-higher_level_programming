5f053f49de47f110fe5a56356b27fd290a5fb044
Natnael04 <natnaeldeboch@gmail.com>
2023-02-02 05:51:07 -0800

    adding file task 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -40,6 +40,6 @@ class Rectangle:
 
     def perimeter(self):
         """to return the perimeter of a rectangle."""
-        return ((self.__width * 2) + (self.__height * 2))
-        if self.__width == 0 and self.__height == 0:
-            return self.__perimeter == 0
+        
+        if self.__width != 0 and self.__height != 0:
+            return ((self.__width * 2) + (self.__height * 2))
