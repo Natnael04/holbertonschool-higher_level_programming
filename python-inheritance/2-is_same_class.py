3123b6371aff8ad4c7cb05357e3c7c3c18613b59
Natnael04 <natnaeldeboch@gmail.com>
2023-02-09 06:45:42 -0800

    adding task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""to check the object an instance of specified class"""
+
+
 def is_same_class(obj, a_class):
+    """to check the object an instance of specified class"""
     return type(obj) is a_class
+
 
 is_same_class(1, int)
 is_same_class(1, object)
