e95075e96dd7a9a8c79ef657986d05858ee55ac3
Natnael04 <natnaeldeboch@gmail.com>
2023-02-10 01:59:17 -0800

    adding task 5

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define the instance of a class"""
+def inherits_from(obj, a_class):
+    """to return the instance of a class that inherited"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
