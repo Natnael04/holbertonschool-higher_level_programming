97cf51f400a4503ed9bdfb3dae0d2f756b6c33f7
Natnael04 <natnaeldeboch@gmail.com>
2023-02-10 10:28:12 -0800

    adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating an empty class"""
+
+
+class BaseGeometry:
+    """to create empty class"""
+    pass
+    """to raise Exception with message"""
+    def area(self):
+        raise Exception("area() is not implemented")
