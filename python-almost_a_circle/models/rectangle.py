4585a0b2f2f3d19f54f2d44486a410c577ad9d40
Natnael04 <natnaeldeboch@gmail.com>
2023-02-13 08:14:06 -0800

    adding task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         else:
             Rectangle.__nb_objects += 1
             self.id = Rectangle.__nb_objects
-    """define get width function"""
+    """ getting width of the Rectangle."""
     def getwidth(self):
         return self.width
 
