f4fef2b703cb442007341e3fd7e81a43adb0632d
Natnael04 <natnaeldeboch@gmail.com>
2023-02-13 08:10:24 -0800

    adding task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""importing base from base file."""
+"""importing base from base file.
+"""
 from models.base import Base
 
-"""creating a rectangle of class "Base" """
+"""creating a rectangle of class "Base". 
+"""
 
 
 class Rectangle(Base):
-    """creating rectangle class"""
+    """creating rectangle class.
+    """
     __nb_objects = 0
-    """define private class attribute."""
+    """define private class attribute.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         if id is not None:
@@ -36,23 +40,23 @@ class Rectangle(Base):
 
     def getx(self):
         return self.x
-    """define private instance attribute with setter."""
+    
 
     def setx(self, value):
         self.x = value
-    """define private instance attribute with getter."""
+    
 
     def gety(self):
         return self.y
-    """define private instance attribute with setter."""
+    
 
     def sety(self, value):
         self.y = value
-    """define private instance attribute with getter."""
+    
 
     def getid(self):
         return self.id
-    """define private instance attribute with setter."""
+    
 
     def setid(self, value):
         self.id = value
