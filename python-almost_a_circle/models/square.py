c0ef372a4fc889d627e4f37cbe4cf9cc9347a7ec
Natnael04 <natnaeldeboch@gmail.com>
2023-02-16 07:08:29 -0800

    updating task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,7 +14,6 @@ class Square(Rectangle):
         """use class constructor to call the super class.
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """the overloading str method to return.
@@ -23,5 +22,18 @@ class Square(Rectangle):
             self.id,
             self.x,
             self.y,
-            self.size
+            self.height
         )
+
+    @property
+    def size(self):
+        """getting self width of a square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setting.
+        """
+        self.height = value
+        self.width = value
