30b5d5769fbd0d3a631dfb3d272c80d4ca458096
Natnael04 <natnaeldeboch@gmail.com>
2023-02-21 06:20:08 -0800

    adding task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -49,12 +49,15 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
+        """Return the perimeter of the Rectangle.
+        """
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
+        """Return the string of the Rectangle.
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
@@ -66,12 +69,14 @@ class Rectangle:
         return ("".join(rect))
 
     def __repr__(self):
-        """Return the string representation of the Rectangle."""
+        """Return the string representation of the Rectangle.
+        """
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
 
     def __del__(self):
-        """Print a message for every deletion of a Rectangle."""
+        """Print a message for every deletion of a Rectangle.
+        """
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
