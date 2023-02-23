07981d8faefcbf72359f0966c42c2253c23fa3ac
Natnael04 <natnaeldeboch@gmail.com>
2023-02-23 05:33:39 -0800

    Removing print test

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -12,7 +12,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """to validate the value is integer"""
+        """to validate the value is integer """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
