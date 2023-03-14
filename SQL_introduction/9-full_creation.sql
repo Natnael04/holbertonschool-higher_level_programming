7ce352465634c761c79e617ebaa863bb70155ffe
Natnael <>
2023-03-14 13:10:13 +0100

    add multiple row

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create a table and add multiple rows
-CREATE TABLE first_table(id INT ,name VARCNSERT, score INT);
-INSERT INTO first_table(id, name, score) VALUES(1, "John", 10);
-INSERT INTO first_table(id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO first_table(id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO first_table(id, name, score) VALUES(4, "George", 8);
+CREATE TABLE second_table(id INT ,name VARCNSERT, score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES(4, "George", 8);
