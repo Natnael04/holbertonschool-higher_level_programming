edd43abd18862a2e4aaf7a2cf37a7823828d89a9
Natnael <>
2023-03-14 17:45:01 +0100

    lists group

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all record of the table by their group
-SELECT score, name
+SELECT 'score', 'name'
 FROM second_table
-WHERE name != ""
-GROUP BY name
-ORDER BY score DESC;
+WHERE 'name' != ""
+GROUP BY 'name'
+ORDER BY 'score' DESC;
