b9fec961c4584910b54d5042a10bf7e0c6af6d0b
Natnael <>
2023-03-14 17:50:38 +0100

    grouping lists

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all record of the table by their group
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' != ""
-ORDER BY 'score' DESC
+SELECT score, name
+FROM  second_table
+WHERE name != ""
+ORDER BY score DESC
