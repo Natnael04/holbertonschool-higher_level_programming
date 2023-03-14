3a524a286f49f13955c450d974ff15e1298a45a3
Natnael <>
2023-03-14 13:47:36 +0100

    multiple row

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create a table and add multiple rows
-CREATE TABLE second_table(id INT ,name VARCHAR(256), score INT);
+CREATE TABLE if not exists second_table(id INT ,name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score) 
 VALUES
     (1, "John", 10),
