b1f87a514d250ec836d3e79411d37b405510f2fc
Natnael <>
2023-03-14 14:41:13 +0100

    lists less than 10

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,5 @@
 -- lists all record with a score greater than or equal to ten
-SELECT score, name FROM second_table ORDER BY score DESC
-WHERE score >= 10;
+SELECT score, name 
+FROM second_table 
+WHERE score >= 10
+ORDER BY score DESC;
