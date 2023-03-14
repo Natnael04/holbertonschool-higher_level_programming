bae7fddfd7f0952f6c62e70e1fb9e6f8f054df80
Natnael <>
2023-03-14 13:06:14 +0100

    create table and insert multiple row

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,5 @@
--- tp create multiple row in the database
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES(1, "John", 10);
-INSERT INTO second_table(id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO second_table(id, name, score) VALUES(4, "George", 8);
-SELECT * FROM second_table;
+CREATE TABLE first_table(id INT ,name VARCNSERT, score INT);
+INSERT INTO first_table(id, name, score) VALUES(1, "John", 10);
+INSERT INTO first_table(id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO first_table(id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO first_table(id, name, score) VALUES(4, "George", 8);
