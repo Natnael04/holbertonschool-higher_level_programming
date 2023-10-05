4a9269cd8e1c41ef299bbc5dcbe64d9ca5847ee2
Natnael <>
2023-10-05 19:46:25 +0200

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import add_0
-
-a = 1
-b = 2
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def add(a, b):
+    return a + b
+if __name__ == "__main__":
+    # Code here will only execute when this script is run directly, not when imported as a module
+    a = 1
+    b = 2
+    result = add(1, 2)
+    print("{} + {} = {}".format(a, b, result))
