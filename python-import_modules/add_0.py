8652d129f198ebea45d57a734b08fded384fecf6
Natnael <>
2023-10-05 17:40:08 +0200

    add

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+if __name__ == "__0-add__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    
