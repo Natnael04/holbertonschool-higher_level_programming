261f975e1ce49cd60fbefbb351a38c439824abc5
Natnael <>
2023-10-05 17:45:23 +0200

    fix error

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-result = add_0.add(1, 2)
-print("{} + {} = {}".format(a, b, result), end="\n")
+if __name__ == "__0-add__":
+   a = 1
+   b = 2
+   result = add_0.add(1, 2)
+   print("{} + {} = {}".format(a, b, result), end="\n")
