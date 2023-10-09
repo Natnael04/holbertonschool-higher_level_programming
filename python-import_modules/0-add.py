d3de685ed3e71dbe28a9a3e16bf32c26e13af6ef
Natnael <>
2023-10-09 17:26:46 +0200

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     # Code here will only execute when this script is run directly, not when imported as a module
     a = 1
     b = 2
-    result = add(1, 2)
+    result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
