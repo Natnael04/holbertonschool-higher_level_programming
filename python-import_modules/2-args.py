03c9f844122ed9a37cc2f405c0057fef1d89a280
Natnael <>
2023-10-09 18:58:59 +0200

    arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,13 @@ import sys
 def main():
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
-    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
-    for i, arg in enumerate(arguments, start=1):
-        print(f"{i}: {arg}")
+
+    if num_arguments == 0:
+        print(f"{num_arguments} arguments.")
+    else:
+        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
 
 
 if __name__ == "__main__":
