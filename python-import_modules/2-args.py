b410ab704db1c962531894ed151251ba04d96a9d
Natnael <>
2023-10-09 18:47:08 +0200

    count arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
+    for i, arg in enumerate(arguments, start=1):
+        print(f"{i}: {arg}")
+
+
+if __name__ == "__main__":
+    main()
