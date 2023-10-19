6a6d8fc5eb632ab9be072d0e430b0cf757b57bbc
Natnael <>
2023-10-19 05:46:41 +0200

    fix the spcae

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, element in enumerate(row):
             print("{:d}".format(element), end=' ')
+            if i < len(row) - 1:
+                print('', end='')
         print()
